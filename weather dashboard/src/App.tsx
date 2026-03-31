9be2a7d9290858bbb499e721486a5ee6de933605
Samuel <samuel.fsbs@gmail.com>
2026-03-31 10:54:17 -0300

    Usando função para usar a localização atual do usuário.

--- weather dashboard/src/App.tsx
+++ weather dashboard/src/App.tsx
@@ -2,13 +2,20 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [Temperatura, SetTemperatura, Cidade, Localização] = useState(25)
+  const [Temperatura, SetTemperatura] = useState(25)
+  const [Cidade, Localização] = useState()
 
   useEffect(() => {
-    const DadosAPI = async () => {
 
+    const Permissao = () => {
+      const Dados = navigation.geolocation.getCurrentPosition.window.prompt("É necessário que você conceda a permissão para usarmos a sua localização atual")
+    };
+
+    Permissao();
+
+    const DadosAPI = async () => {
       try {
-        const Dados = (await fetch(' /* Exemplo de API */ '));
+        const Dados = (await fetch('https://weather.com/weather/today/l/5aea1d50a6d6b9e99cf89ba79f463d67dcf21ea5061990aae1ffc1c7fa8911a9'));
         const Dadosformatados = await Dados.json();
         SetTemperatura(DadosFomatados);
       } catch (error) {
