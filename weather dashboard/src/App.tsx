1ff264dd3b9f5522e9e43e090dc7dd0b7427a649
Samuel <samuel.fsbs@gmail.com>
2026-03-31 10:23:44 -0300

    Criando variáveis com estado.

--- weather dashboard/src/App.tsx
+++ weather dashboard/src/App.tsx
@@ -2,12 +2,24 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [Temperatura, SetTemperatura] = useState(25)
+  const [Temperatura, SetTemperatura, Cidade, Localização] = useState(25)
 
   useEffect(() => {
+    const DadosAPI = async () => {
 
-  }, []);
+      try {
+        const Dados = (await fetch(' /* Exemplo de API */ '));
+        const Dadosformatados = await Dados.json();
+        SetTemperatura(DadosFomatados);
+      } catch (error) {
+        console.log("Houve um erro ao buscar as informações " + error)
+      }
 
+    };
+
+    DadosAPI();
+
+  }, [Cidade]);
 
   return (
     <>
